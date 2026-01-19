b9391d0d9158b6a96acefc836dba596d53de69f4
denisseleznev1 <1denisseleznev1@gmail.com>
2026-01-19 22:24:42 +0600

    :)

--- dog-app/src/App.jsx
+++ dog-app/src/App.jsx
@@ -6,8 +6,8 @@ import './App.css'
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/image/:url" element={<ImagePage />} />
+      <Route path="/mobdev-lab12-SELEZNEV/" element={<Home />} />
+      <Route path="/mobdev-lab12-SELEZNEV/image/:url" element={<ImagePage />} />
     </Routes>
   )
 }
