7438b29e2c4214a1a22c04b98428ac97e8c786e5
denisseleznev1 <1denisseleznev1@gmail.com>
2026-01-19 22:44:29 +0600

    >:|

--- dog-app/src/main.jsx
+++ dog-app/src/main.jsx
@@ -1,13 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { RouterProvider } from 'react-router-dom'
+import { router } from './router'
+import { TelegramProvider } from './context/TelegramContext'
 import './index.css'
-import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <TelegramProvider>
+      <RouterProvider router={router} />
+    </TelegramProvider>
+  </React.StrictMode>,
 )
